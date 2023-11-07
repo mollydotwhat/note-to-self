ea9a7f583ff820ec081f6b694391fb8d24ea5f88
molly <molly.salzberger@gmail.com>
2023-11-07 08:35:30 -0600

    this looks right, but is it? requirements don't SAY i have to get one note at a time, and the public index.js looks like it handles that. have to finish setting up routes to test, and I think I'm missing something still...

--- routes/notes.js
+++ routes/notes.js
@@ -10,6 +10,27 @@ const {
 
   //methods go here
 // GET /api/notes (read json)
+notes.get('/', (req, res) => {
+    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+  });
 // POST /api/notes (write to json, return object/new note. uuid needed here.)
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/notes.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
   module.exports = notes;
