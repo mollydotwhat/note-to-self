38c5fedc996af7f719b1f791bc4e93dbb49eb34a
molly <molly.salzberger@gmail.com>
2023-11-07 08:43:04 -0600

    uncommented app.use('/api/notes', notesRouter); in the routes index, and it hasn't changed anything so far. Signs point to redundant.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const notesRouter = require('./notes')
 //and this is what makes me think I've set up the routes wrong
 
 const app = express();
-//app.use('/api/notes', notesRouter); 
+app.use('/api/notes', notesRouter); 
 // this works if it's the one where i can just specify the URL, and tell it where to look for the routes. but i think this doesn't go here?  I think it's defining the routes folder in the server.
 
 
