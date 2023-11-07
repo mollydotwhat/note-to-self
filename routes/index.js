91f480b407a475d652bc8d3dfaad46319a5d9e2c
molly <molly.salzberger@gmail.com>
2023-11-07 08:24:06 -0600

    server writing. maybe the notes form doesn't have enough fields for this to make sense? does it matter if it's redundant if i can get it to work?

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const notesRouter = require('./notes')
 //and this is what makes me think I've set up the routes wrong
 
 const app = express();
-app.use('/api/notes', notesRouter);
-// this works if it's the one where i can just specify the URL, and tell it where to look for the routes. Probably the first thing I should check if I get a GET error.
+//app.use('/api/notes', notesRouter); 
+// this works if it's the one where i can just specify the URL, and tell it where to look for the routes. but i think this doesn't go here?  I think it's defining the routes folder in the server.
 
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./routes/index.js');
+//should this be here as well as the helper file?? if it works in the routes i don't know what i'd use fs here for.
+// const fs = require('fs');
+const api = require('./routes/index.js');
 // not sure routes folder is correct--maybe just one called api?
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
