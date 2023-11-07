746c74b78f8f37ba784ca13a8c719bccedd1b98f
molly <molly.salzberger@gmail.com>
2023-11-07 08:04:04 -0600

    planned a file structure, but putting it up i am now uncertain that it's what's intended for this project.

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+// const api = require('./routes/index.js');
+// not sure routes folder is correct--maybe just one called api?
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
