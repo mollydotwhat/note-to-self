8142fc8b821851a748b655083f41786602e11c32
molly <molly.salzberger@gmail.com>
2023-11-07 08:09:10 -0600

    importing methods to read/write

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,7 @@
 const notes = require('express').Router();
+//actually putting things into the json
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
